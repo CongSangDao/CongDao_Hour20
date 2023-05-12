26c236777777bf62ab538dd2e61646b023600e6f
CongSangDao <sangdaovnb2002@gmail.com>
2023-05-11 19:13:39 -0700

    Initial commit

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject powerupPrefab;
+    public GameObject obstaclePrefab;
+    public float spawnCycle = .5f;
+
+    GameManager manager;
+    float elapsedTime;
+    bool spawnPowerup = true;
+
+    void Start()
+    {
+        manager = GetComponent<GameManager>();
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime > spawnCycle)
+        {
+            GameObject temp;
+            if (spawnPowerup)
+                temp = Instantiate(powerupPrefab) as GameObject;
+            else
+                temp = Instantiate(obstaclePrefab) as GameObject;
+
+            Vector3 position = temp.transform.position;
+            position.x = Random.Range(-3f, 3f);
+            temp.transform.position = position;
+
+            Collidable col = temp.GetComponent<Collidable>();
+            col.manager = manager;
+
+            if(!spawnPowerup)
+            {
+                float scale = Random.Range(0.5f, 4f);
+                temp.transform.localScale = new Vector3(scale, scale, scale);
+            }
+
+            elapsedTime = 0;
+            spawnPowerup = !spawnPowerup;
+        }
+    }
+}
